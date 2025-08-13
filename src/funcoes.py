98ee8300307f72614c514e23e68acf825517610a
Ryan1916 <lira.yryan@gmail.com>
2025-08-13 20:32:14 -0300

    funcoes.py

---
+++ src/funcoes.py
@@ -0,0 +1,11 @@
+passos={
+    "Pegar 1 ovo",
+    "Aquecer a frigideira",
+    "Adicionar um fio de óleo",
+    "Quebrar o ovo na frigideira",
+    "Cozinhar por alguns minutos",
+    "Servir"
+} 
+ 
+for i, passo in enumerate(passos, start=1):
+  print(f"{i},{passo}") 
